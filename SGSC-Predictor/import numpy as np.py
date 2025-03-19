463c9ffda4bd640426e1ceab8a71384c6ddfa0fb
Kushagra Sharma <114687412+bhardwaj-kushagra@users.noreply.github.com>
2025-03-20 02:42:47 +0530

    Update import numpy as np.py

--- SGSC-Predictor/import numpy as np.py
+++ SGSC-Predictor/import numpy as np.py
@@ -3,7 +3,7 @@ import joblib
 from sklearn.preprocessing import StandardScaler
 
 # Load the original scaler
-scaler = joblib.load('scaler.pkl')
+scaler = joblib.load('scaler.pkl') #saved XGboost Model
 
 # Save it again with an updated format
 joblib.dump(scaler, 'scaler_fixed.pkl') #ur file name
